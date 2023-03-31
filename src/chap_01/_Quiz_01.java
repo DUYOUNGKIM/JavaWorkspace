772e49868f437f353eacf580f6071a4970e272f5
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-03-31 18:12:38 +0900

    자바퀴즈.01

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,18 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNo = "광명22";
+        int time = 3;
+
+        double distance = 1.5;
+
+        System.out.println(busNo+"번 버스");
+        System.out.println(time+"분 후 도착");
+        System.out.println(distance+"km");
+    }
+
+
+
+
+}
