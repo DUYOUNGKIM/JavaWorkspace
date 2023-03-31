f62bae55d16ce13cd97fcc7b5a681f8b40a9de87
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-03-31 16:47:00 +0900

    자료형

--- src/chap_01/_01_DataTypes.java
+++ src/chap_01/_01_DataTypes.java
@@ -1,2 +1,11 @@
-package chap_01;public class _01_DataTypes {
+package chap_01;
+
+public class _01_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+        System.out.println("안녕하세요?");  //문자열 자료형
+        System.out.println(12);           //숫자 자료형
+        System.out.println(true);        //boolean 자료형
+        System.out.println(false);      //boolean 자료형
+    }
 }
