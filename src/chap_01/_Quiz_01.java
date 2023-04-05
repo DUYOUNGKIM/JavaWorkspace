f81cc1881330100cc7adbb7b332cd03879f1f6ca
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-04-05 12:13:40 +0900

    자료형 변수 연산자 복습

--- src/chap_01/_Quiz_01.java
+++ src/chap_01/_Quiz_01.java
@@ -1,18 +1,24 @@
 package chap_01;
-
+/*
+(실행 결과)
+상암08번 버스
+약 3분 후 도착
+남은 거리 1.2km
+ */
 public class _Quiz_01 {
     public static void main(String[] args) {
-        String busNo = "광명22";
-        int time = 3;
+        // 버스 번호 (1234, 상암08)
+        String busNo = "상암08";
 
-        double distance = 1.5;
+        // 남은 시간 (3분, 5분)
+        int minute = 3;
 
-        System.out.println(busNo+"번 버스");
-        System.out.println(time+"분 후 도착");
-        System.out.println(distance+"km");
+        // 남은 거리 (1.5km, 0.8km)
+        double distance = 1.2;
+
+        // 결과 출력
+        System.out.println(busNo + "번 버스");
+        System.out.println("약 " + minute + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
     }
-
-
-
-
 }
