d976854d8baa4545fbba380ad7d164b2a9a2b552
DUYOUNGKIM <110887144+DUYOUNGKIM@users.noreply.github.com>
2023-04-12 13:44:47 +0900

    배열

--- src/chap_01/_01_HelloWorld.java
+++ src/chap_01/_01_HelloWorld.java
@@ -3,5 +3,18 @@ package chap_01;
 public class _01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("안녕하세요?");
+        int num =1;
+        for (int i=1; i<=100; i++){
+            if (i%3==0){
+
+                System.out.println(num +"번째 3의배수"+ i);
+                num +=1;
+            }
+        }
+
     }
-}
+
+        }
+
+
+
